1889513500db7870ba6cbb0896f5f321c2ba23f8
Mark van der Wal <wilds.mark@gmail.com>
2018-03-29 23:45:34 +0200

    made block and chunk class

---
+++ Assets/Scripts/World/Block.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+
+public class Block
+{
+
+    private float tilesizeUV;
+    public Vector3 Position { get; private set; }
+    public bool isSolid { get; set; }
+
+    public Block(Vector3 pos, bool solid)
+    {
+        Position = pos;
+        isSolid = solid;
+    }
+
+    public Mesh build(Transform transform, int tileIndex, float tileSizeNormalized)
+    {
+        tilesizeUV = tileSizeNormalized;
+        Mesh cubeMesh = new Mesh();
+        CombineInstance[] quads = new CombineInstance[6];
+        quads[0] = createQuad(transform, Vector3.back, tileIndex);
+        quads[1] = createQuad(transform, Vector3.forward, tileIndex);
+        quads[2] = createQuad(transform, Vector3.left, tileIndex);
+        quads[3] = createQuad(transform, Vector3.right, tileIndex);
+        quads[4] = createQuad(transform, Vector3.down, tileIndex);
+        quads[5] = createQuad(transform, Vector3.up, tileIndex);
+
+        cubeMesh.CombineMeshes(quads);
+
+        return cubeMesh;
+    }
+
+    private CombineInstance createQuad(Transform transform, Vector3 planeNormal, int tileIndex)
+    {
+        CombineInstance combine = new CombineInstance();
+        combine.transform = transform.localToWorldMatrix * Matrix4x4.Translate(Position);
+        combine.mesh = new Mesh
+        {
+            vertices = createPositions(planeNormal),
+            uv = createUVArray(tileIndex),
+
+            normals = new[]
+            {
+                planeNormal, planeNormal, planeNormal, planeNormal
+            },
+
+            triangles = new[]
+            {
+                0, 1, 2,
+                2, 3, 0
+            }
+        };
+
+        return combine;
+    }
+
+    private Vector3[] createPositions(Vector3 planeNormal)
+    {
+        float size = 0.5f;
+        float dot = Vector3.Dot(Vector3.up, planeNormal);
+        bool isNotParallel = Mathf.Abs(dot) < 1.0f - Mathf.Epsilon;
+
+        Vector3 helperAxis = isNotParallel ? Vector3.up : Vector3.right;
+        Vector3 xAxis = Vector3.Cross(planeNormal, helperAxis);
+        Vector3 yAxis = Vector3.Cross(xAxis, planeNormal);
+
+//        xAxis *= size;
+//        yAxis *= size;
+//        planeNormal *= size;
+
+        return new[]
+        {
+            -xAxis + -yAxis + planeNormal,
+            -xAxis + yAxis + planeNormal,
+            xAxis + yAxis + planeNormal,
+            xAxis + -yAxis + planeNormal
+        };
+    }
+
+    private Vector2[] createUVArray(int tileIndex)
+    {
+        float uvX = (tileIndex % 16) * tilesizeUV;
+        float uvY = tileIndex / 16 * tilesizeUV;
+
+        return new[]
+        {
+            new Vector2(uvX, uvY),
+            new Vector2(uvX, uvY + tilesizeUV),
+            new Vector2(uvX + tilesizeUV, uvY + tilesizeUV),
+            new Vector2(uvX + tilesizeUV, uvY)
+        };
+    }
+}
---
+++ Assets/Scripts/World/Chunk.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chunk : MonoBehaviour {
+    public int tilesetWidth = 16;
+    public float tilesetDimension = 512.0f;
+    public int tilesetIndex = 0;
+
+    private Block[,,] blocks;
+    public Material material;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(build(16,8,16));
+    }
+
+    public IEnumerator build( int width, int height, int depth)
+    {
+        blocks = new Block[width, height, depth];
+        
+        Mesh chunkMesh = new Mesh();
+        CombineInstance[] blockMeshes = new CombineInstance[width * height * depth];
+
+        for (int z = 0; z < depth; z++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    Vector3 position = new Vector3(x, y, z);
+                    Block newBlock = new Block(position, Random.value < 0.5f);
+                    blocks[x, y, z] = newBlock;
+                }
+            }
+        }
+
+        for (int z = 0; z < depth; z++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    if(blocks[x, y, z].isSolid)
+                        continue;
+                    
+                    int indexCombined = x + y * width + z * height * width;
+                    blockMeshes[indexCombined].mesh = blocks[x, y, z].build(transform, tilesetIndex, tilesetWidth / tilesetDimension);
+                    blockMeshes[indexCombined].transform = Matrix4x4.Translate(blocks[x, y, z].Position);
+                }
+            }
+        }
+
+        chunkMesh.name = "Chunk";
+        chunkMesh.CombineMeshes(blockMeshes);
+        chunkMesh.RecalculateBounds();
+
+        gameObject.AddComponent<MeshRenderer>().material = material;
+        MeshFilter meshFilter = gameObject.AddComponent<MeshFilter>();
+        meshFilter.mesh = chunkMesh;
+
+        yield return null;
+    }
+}
